ad61ac8d001bd80cc49039179902b368bdb78228
Sreyas Krishna Natarajan <snatarajan@royal-13.cs.wisc.edu>
2017-11-01 18:13:22 -0500

    first commit

---
+++ xv6/user/hello.c
@@ -0,0 +1,19 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+
+int
+main(int argc, char *argv[])
+{
+
+char *ptr = (char *)0;
+
+printf(1,"&ptr = %x \n" , ptr);
+printf(1,"ptr = %c", *ptr);
+
+exit();
+
+}
+
+
